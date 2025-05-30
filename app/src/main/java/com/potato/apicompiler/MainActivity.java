5282b5edd038e7e259743e5ec9ad146967bde058
Potato <7638657+potato-2020@user.noreply.gitee.com>
2025-05-30 14:39:49 +0800

    构建文件

--- app/src/main/java/com/potato/apicompiler/MainActivity.java
+++ app/src/main/java/com/potato/apicompiler/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.potato.tools.DebugManager;
-
 public class MainActivity extends AppCompatActivity {
 
     @ApiLog(nameChinese = "初始化", nameEnglish = "onCreate()")
